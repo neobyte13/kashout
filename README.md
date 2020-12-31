af6d2ba9247f60d0951897c06cadcf1c854ace23
neobyte13 <neobyte13@gmail.com>
2020-12-31 14:44:28 +0100

    initial empty project with appicon

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kashout
+
+A mobile fintech project.
---
+++ android/app/src/main/kotlin/com/daniel/kashout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.daniel.kashout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
